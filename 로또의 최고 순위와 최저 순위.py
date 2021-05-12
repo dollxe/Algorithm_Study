14d4f079c73a372d418acf938f50522130938130
LALA <50886834+dollxe@users.noreply.github.com>
2021-05-12 21:14:28 +0900

    Update 로또의 최고 순위와 최저 순위.py

--- 로또의 최고 순위와 최저 순위.py
+++ 로또의 최고 순위와 최저 순위.py
@@ -4,15 +4,15 @@ def solution(lottos, win_nums):
     win_nums = set(win_nums)
     match = 0
     _0_cnt = 0
-    
 
     for num in lottos:
         if num in win_nums: match += 1
         elif num == 0: _0_cnt += 1
 
-    top_match = 1 if match + _0_cnt >= 6 else 7 - (match + _0_cnt)
-    bottom_match = 6 if match == 0 else 7 - match
+    # if  match == 0 and _0_cnt == 0: return [6,6]
+    top_match = min(6, 7 - (match + _0_cnt))
+    bottom_match = min(6, 7 - match)
     return [top_match, bottom_match]
 
 if __name__ == '__main__':
-    print(solution([44, 0, 0, 0, 0, 0], [31, 2, 44, 1, 6, 25]))
+    print(solution([18, 19, 13, 3, 17, 45], [31, 2, 44, 1, 6, 25]))
